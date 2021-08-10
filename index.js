64e3613509ddd17f8a3ce4dfe090fa8599163146
+ <+>
2021-08-10 08:21:05 -0700

    commit tree 1e1d86ec097f21134dc2bb7a995bcdf87afe50b9

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 0;
+
+const mod = num3 % num4
+const num3 = 16
+const num4 = 6
